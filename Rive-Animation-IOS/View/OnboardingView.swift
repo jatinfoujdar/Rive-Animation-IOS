89f4e04412d0dc37d4e112a99dcca409051b1321
Jatin Foujdar <jatin321001@gmail.com>
2024-12-27 17:49:07 +0530

    rive onboarding view  commit

--- Rive-Animation-IOS/View/OnboardingView.swift
+++ Rive-Animation-IOS/View/OnboardingView.swift
@@ -3,15 +3,28 @@ import RiveRuntime
 
 
 struct OnboardingView: View {
+    let button = RiveViewModel(fileName: "button")
     var body: some View {
-        RiveViewModel(fileName: "shapes").view()
-            .ignoresSafeArea()
-            .blur(radius: 30)
-            .background(
-                Image("Spline")
-                    .blur(radius: 50)
-                    .offset(x:200,y: 100)
-            )
+        ZStack{
+            RiveViewModel(fileName: "shapes").view()
+                .ignoresSafeArea()
+                .blur(radius: 30)
+                .background(
+                    Image("Spline")
+                        .blur(radius: 50)
+                        .offset(x:200,y: 100)
+                )
+             button.view()
+                .frame(width: 236,height: 64)
+                .overlay(
+                    Label("Start the course",systemImage: "arrow.forward")
+                        .offset(x:4,y:4)
+                        .font(.headline)
+                )
+                .onTapGesture {
+                button.play(animationName: "active")
+                }
+        }
     }
 }
 
