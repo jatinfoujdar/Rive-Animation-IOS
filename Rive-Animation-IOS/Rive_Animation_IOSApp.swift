d456fbdd02973aedbbdcedc0f2db0a47a926b2e6
Jatin Foujdar <jatin321001@gmail.com>
2024-12-27 13:17:04 +0530

    Initial Commit

---
+++ Rive-Animation-IOS/Rive_Animation_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rive_Animation_IOSApp.swift
+//  Rive-Animation-IOS
+//
+//  Created by jatin foujdar on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Rive_Animation_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
