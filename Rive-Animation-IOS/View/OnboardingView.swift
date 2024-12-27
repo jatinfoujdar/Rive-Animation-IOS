94d569d1179e0ec60c415eb9ce05bef4f961e06a
Jatin Foujdar <jatin321001@gmail.com>
2024-12-27 17:41:08 +0530

    onboarding commit

---
+++ Rive-Animation-IOS/View/OnboardingView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import RiveRuntime
+
+
+struct OnboardingView: View {
+    var body: some View {
+        RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+                Image("Spline")
+                    .blur(radius: 50)
+                    .offset(x:200,y: 100)
+            )
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
