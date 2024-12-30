e31326abae62d222eca00d7e4604c48ad27855fb
Jatin Foujdar <jatin321001@gmail.com>
2024-12-30 23:34:54 +0530

    integrated gemini AI in app commit

--- Rive-Animation-IOS/ContentView.swift
+++ Rive-Animation-IOS/ContentView.swift
@@ -1,21 +1,57 @@
-//
-//  ContentView.swift
-//  Rive-Animation-IOS
-//
-//  Created by jatin foujdar on 27/12/24.
-//
-
 import SwiftUI
+import GoogleGenerativeAI
 
 struct ContentView: View {
+    let model = GenerativeModel(name: "gemini-pro", apiKey: ApiKey.default)
+    
+    @State var userPrompt = ""
+    @State var response = "How can I help you today?"
+    @State var isLoading = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to Gemini Ai")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            ZStack{
+                ScrollView{
+                    Text(response)
+                        .font(.title)
+                }
+                if isLoading{
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .scaleEffect(4)
+                }
+            }
+            TextField("Ask Anything...", text: $userPrompt, axis: .vertical)
+                .lineLimit(5)
+                .font(.title)
+                .padding()
+                .background(Color.indigo.opacity(0.2), in: Capsule())
+                .disableAutocorrection(true)
+                .onSubmit {
+                    generateResponse()
+                }
         }
         .padding()
+    }
+    func generateResponse(){
+        isLoading = true
+        response = ""
+        
+        Task{
+            do{
+                let result = try await model.generateContent(userPrompt)
+                isLoading = false
+                response = result.text ?? "NO response Found"
+                userPrompt = ""
+            }
+            catch{
+                response = "somthig went wrong"
+            }
+        }
     }
 }
 
