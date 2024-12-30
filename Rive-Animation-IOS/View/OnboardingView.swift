6c4bcac3f5993e7c6739b372b6df8fc03df2d993
Jatin Foujdar <jatin321001@gmail.com>
2024-12-30 14:44:33 +0530

    setup onboarding commit

--- Rive-Animation-IOS/View/OnboardingView.swift
+++ Rive-Animation-IOS/View/OnboardingView.swift
@@ -15,19 +15,23 @@ struct OnboardingView: View {
                         .offset(x:200,y: 100)
                 )
             VStack(alignment: .leading, spacing: 16){
-                Text("Learn Design & Code")
+                Text("Recipe Buddy")
                     .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
                     .frame(width: 260, alignment: .leading)
                 
-                Text("Don't skip design. Learn design and code, by building real apps with React and Swift. Complete courses about best tools.")
-                    .customFont()
+                Text("Recipe Buddy is your ultimate cooking companion! Simply enter a recipe, get a complete ingredient list, and add everything to your quick commerce cart with a single tap. Meal prep has never been this easy!")
+                    .font(.headline)
+                
+                Text("Whisk it, list it, shop it—your recipe adventure starts here!")
+                    .font(.title2)
+                    .fontWeight(.bold)
                 
                 Spacer()
                 
                 button.view()
                     .frame(width: 236,height: 64)
                     .overlay(
-                        Label("Start the course",systemImage: "arrow.forward")
+                        Label("Let’s get cookin’!",systemImage: "arrow.forward")
                             .offset(x:4,y:4)
                             .font(.headline)
                     )
