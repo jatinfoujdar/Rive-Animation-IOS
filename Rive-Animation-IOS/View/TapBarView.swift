98c72bf06dd54e1f080499402f80ab5adac010bb
Jatin Foujdar <jatin321001@gmail.com>
2024-12-31 14:22:50 +0530

    navbar/toolbar commit

--- Rive-Animation-IOS/View/TapBarView.swift
+++ Rive-Animation-IOS/View/TapBarView.swift
@@ -2,23 +2,49 @@ import SwiftUI
 import RiveRuntime
 
 struct TapBarView: View {
-    let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
-    
     var body: some View {
-        HStack{
-            Button{
-                icon.setInput("active",value: true)
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1 ){
-                    icon.setInput("active", value: false)
+        VStack{
+            Spacer()
+            HStack{
+                ForEach(tabItems) { item in
+                    Button {
+                        item.icons.setInput("active", value: true)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            item.icons.setInput("active", value: false)
+                        }
+                    } label: {
+                        item.icons.view()
+                            .frame(height: 36)
+                    }
                 }
-            }label:{
-                icon.view()
             }
+            .padding(12)
+            .background(Color("Background 2").opacity(0.8))
+            .background(.ultraThinMaterial)
+            .mask(RoundedRectangle(cornerRadius: 24, style: .continuous))
+            .shadow(color: Color("Background 2").opacity(0.3),radius: 20, x: 0, y: 20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 24, style: .continuous)
+                    .stroke(.linearGradient(colors: [.white.opacity(0.5), .white.opacity(0)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            )
+            .padding(.horizontal,24)
         }
-        .background(Color("Background 2").opacity(0.8))
     }
 }
 
 #Preview {
     TapBarView()
 }
+
+struct TabItem: Identifiable {
+    var id = UUID()
+    var icons: RiveViewModel
+}
+
+var tabItems = [
+    TabItem(icons: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")),
+    TabItem(icons: RiveViewModel(fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH")),
+    TabItem(icons: RiveViewModel(fileName: "icons", stateMachineName: "TIMER_Interactivity", artboardName: "TIMER")),
+    TabItem(icons: RiveViewModel(fileName: "icons", stateMachineName: "BELL_Interactivity", artboardName: "BELL")),
+    TabItem(icons: RiveViewModel(fileName: "icons", stateMachineName: "USER_Interactivity", artboardName: "USER"))
+]
