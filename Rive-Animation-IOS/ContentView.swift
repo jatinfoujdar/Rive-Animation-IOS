88dd7207327044e1111baca23ff4e069284c2e3d
Jatin Foujdar <jatin321001@gmail.com>
2025-01-01 15:49:43 +0530

    home update commit

--- Rive-Animation-IOS/ContentView.swift
+++ Rive-Animation-IOS/ContentView.swift
@@ -4,18 +4,24 @@ struct ContentView: View {
     @AppStorage("selectTab") var selectTab : Tab = .chat
     var body: some View {
         ZStack{
-            switch selectTab {
-            case .chat:
-                Text("Chat")
-            case .search:
-                Text("Search")
-            case .timer:
-                Text("Timer")
-            case .bell:
-                Text("Bell")
-            case .user:
-                Text("User")
+            Group{
+                switch selectTab {
+                case .chat:
+                    HomeView()
+                case .search:
+                    Text("Search")
+                case .timer:
+                    Text("Timer")
+                case .bell:
+                    Text("Bell")
+                case .user:
+                    Text("User")
+                }
             }
+            .safeAreaInset(edge: .bottom){
+                Color.clear.frame(height: 80)
+            }
+            
             TapBarView()
         }
     }
