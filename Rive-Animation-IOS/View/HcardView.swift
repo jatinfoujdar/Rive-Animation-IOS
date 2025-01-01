57b5600fe2d68b4f1d257fa352eae7382aca09ec
Jatin Foujdar <jatin321001@gmail.com>
2025-01-01 15:00:31 +0530

    Hcard view commit

--- Rive-Animation-IOS/View/HcardView.swift
+++ Rive-Animation-IOS/View/HcardView.swift
@@ -1,11 +1,28 @@
 import SwiftUI
 
 struct HcardView: View {
+    var section : CourseSection
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 8){
+            VStack(alignment: .leading, spacing: 8){
+                Text(section.title)
+                    .font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(section.caption)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            Divider()
+            section.image
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: 110)
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
     }
 }
 
 #Preview {
-    HcardView()
+    HcardView(section: courseSections[0])
 }
